bc15686cf1fa407de43f91b40d6dd60cc0035b7b
Matt <mammone@gmail.com>
2016-09-21 14:53:02 -0700

    Resolve confilcts

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,6 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController} from 'ionic-angular';
 import {AuthProvider} from "../../providers/auth-provider";
+import {Toast} from 'ionic-native';
+import {PlatformProvider} from "../../providers/platform-provider";
+import {CalendarPage} from '../calendar/calendar';
+import {Department} from "../../models/department";
+import {DepartmentProvider} from "../../providers/department-provider";
+
+/*
+ Generated class for the LoginPage page.
 
 @Component({
   templateUrl: 'build/pages/login/login.html',
@@ -8,11 +16,15 @@ import {AuthProvider} from "../../providers/auth-provider";
 export class LoginPage {
   public loginMode:string = "login";
 
-  constructor(private navCtrl:NavController, private authProvider:AuthProvider) {
+  constructor(private navCtrl:NavController,
+                private authProvider:AuthProvider,
+                private platformProvider:PlatformProvider,
+                private alertCtrl:AlertController,
+                private departmentProvider:DepartmentProvider) {
+
   }
 
   private logUserIn:Function = function () {
     this.authProvider.login();
   }
 }
-
