99a9501d6a783fa09b6ecedfbe0eec9d08a4b593
Matt <mammone@gmail.com>
2016-09-21 14:53:02 -0700

    Initial implementation of registration page

--- app/pages/login/login.ts
+++ app/pages/login/login.ts
@@ -1,18 +1,39 @@
 import { Component } from '@angular/core';
-import { NavController} from 'ionic-angular';
+import { NavController, NavParams} from 'ionic-angular';
 import {AuthProvider} from "../../providers/auth-provider";
+import {Toast} from 'ionic-native';
+import {PlatformProvider} from "../../providers/platform-provider";
+import {CalendarPage} from '../calendar/calendar';
+import {Department} from "../../models/models";
+import {DepartmentProvider} from "../../providers/department-provider";
 
+/*
+ Generated class for the LoginPage page.
+*/
 @Component({
   templateUrl: 'build/pages/login/login.html',
 })
 export class LoginPage {
   public loginMode:string = "login";
 
-  constructor(private navCtrl:NavController, private authProvider:AuthProvider) {
+  selectedRank: string;
+  department : Department;
+  ranks : string[];
+
+  constructor(  private navCtrl:NavController,
+                private navParams: NavParams,
+                private authProvider:AuthProvider,
+                private platformProvider:PlatformProvider,
+                private departmentProvider:DepartmentProvider) {
+
+
+        departmentProvider.Department().subscribe((dept) => {
+          this.department = dept;
+          this.ranks = this.department.ranks;
+        });
   }
 
   private logUserIn:Function = function () {
     this.authProvider.login();
   }
 }
-
